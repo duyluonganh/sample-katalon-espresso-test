25339266ecc60da27d78cbae3ff7c8be0da91c13
Duy Luong <duyluong@kms-technology.com>
2022-03-02 14:06:05 +0700

    Update

--- Scripts/New Test Case/Script1640150669713.groovy
+++ Scripts/New Test Case/Script1640150669713.groovy
@@ -3,12 +3,14 @@ import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import util.SystemUtil
 
 
+//Update your jdk location
 String jdkLocation = '/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home/jre'
 
 SystemUtil.setEnv('JAVA_HOME', jdkLocation)
 
 println System.getenv("JAVA_HOME")
 
+//Update your app location
 Mobile.startApplication('/Users/duyanhluong/Downloads/TheApp-v1.5.0 (3).apk', false)
 
 Mobile.closeApplication()
